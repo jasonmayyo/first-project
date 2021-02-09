345f7f78bfb0ebf7b2c301a517521e04ca0ee1b7
Jason Mayo <jason.m1122332@gmail.com>
2021-02-09 13:45:39 +0200

    Added all peoject files

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var answerNode = document.querySelector('.answer')
+//var footer = document.querySelector('.footer')
+//var notes = document.querySelector('.notes')
+//var footer2 = document.querySelector('.footer2')
+//var progressBar = document.querySelector('.progressBar')
+//var questionNode = document.querySelector('.question')
+
+//footer.addEventListener('click', function(){
+    //answerNode.innerHTML = `A: ${answer}`
+    //notes.style.display = "flex"
+    //footer.style.display = "none"
+    //footer2.style.display = "flex"
+    //progressBar.style.width = "40%"
+    //progressBar.classList.add("pb-transion")
+    //questionNode.classList.add("question-transion")
+    //answerNode.classList.add("answer-transion")
+//})
+
+//answer = "Mitocondria"
+
+
+//TODO LIST:
+
+//var checkbox = document.querySelector(".bigbox")
+//var todoProgressBar = document.querySelector(".todo-progressBar")
+//var todoText = document.querySelector(".todo-text")
+
+//checkbox.addEventListener('click', function(){
+    //todoProgressBar.classList.add("todo-pb-transion")
+    //todoText.classList.add("strike")
+//})
+
+
